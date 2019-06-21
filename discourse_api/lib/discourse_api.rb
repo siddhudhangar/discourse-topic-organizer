9e47175f3e0f7f58f0c3666105e874615e3e674f
jineetd <f20170168@goa.bits-pilani.ac.in>
2019-06-21 16:31:02 +0530

    array exp

--- discourse_api/lib/discourse_api.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-require "discourse_api/api/params"
-require "discourse_api/client"
-require "discourse_api/error"
-require "discourse_api/version"
-require "discourse_api/single_sign_on"
--- discourse_api/lib/discourse_api/version.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-module DiscourseApi
-  VERSION = "0.35.0"
-end
